a0cb193d4df214e891047705b24d6b6cbaed650d
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-03-07 04:51:33 +0600

    include login

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { json } from "express";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
@@ -42,8 +43,31 @@ export const signup = async (req, res) => {
         res.status(500).json({message: "Internal server error"});
     }
 }
-export const login = (req, res) => {
-    res.send("login route")
+export const login = async (req, res) => {
+    const {email, password} = req.body
+    try {
+        const user = await User.findOne({email})
+
+        if(!user) {
+            return res.status(400).json({message: "Invalid credentials"})
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        if(!isPasswordCorrect) {
+            return res.status(400).json({message: "Invalid credentials"})
+        }
+        generateToken(user._id,res)
+        
+        res.status(200).json({
+            _id:user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic, 
+        })
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+    }
 }
 export const logout = (req, res) => {
     res.send("logout route")
