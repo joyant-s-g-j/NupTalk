0430368379056ecbf8b6db7c3440b1190c00529e
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-03-11 09:28:15 +0600

    added profile page

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -89,7 +89,11 @@ export const updateProfile = async (req, res) => {
         }
 
         const uploadResponse = await cloudinary.uploader.upload(profilePic)
-        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic:uploadResponse.secure_url}, {new:true})
+        const updatedUser = await User.findByIdAndUpdate(
+            userId, 
+            { profilePic:uploadResponse.secure_url }, 
+            { new:true }
+        )
 
         res.status(200).json(updatedUser)
     } catch (error) {
