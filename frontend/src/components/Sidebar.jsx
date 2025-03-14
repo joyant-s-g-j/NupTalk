97bec7e6274abd1dc75b42c062d274f464e35ce0
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-03-14 06:24:55 +0600

    home page sidebar fix

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,11 +2,12 @@ import React, { useEffect } from 'react'
 import { useChatStore } from '../store/useChatStore'
 import SidebarSkeleton from './skeletons/SidebarSkeleton';
 import { User } from 'lucide-react';
+import { useAuthStore } from '../store/useAuthStore';
 
 const Sidebar = () => {
   const { getUsers, users, selectedUser, setSelectedUser, isUsersLoading } = useChatStore();
 
-  const onlineUsers = [];
+  const {onlineUsers} = useAuthStore();
   
   useEffect (() => {
     getUsers()
